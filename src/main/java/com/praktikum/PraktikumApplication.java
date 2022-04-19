098d485b0b5513b85dd91cb7481ab583e56ce139
Ilham <muhammadilhamhidayat3@gmail.com>
2022-04-19 23:34:12 +0700

    add GetMapping

--- src/main/java/com/praktikum/PraktikumApplication.java
+++ src/main/java/com/praktikum/PraktikumApplication.java
@@ -6,9 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PraktikumApplication {
 
-
+    @GetMapping("/section-39")
+    public String sayHello() {
+        return "Jangan Dibaca";
+    }
     public static void main(String[] args) {
         SpringApplication.run(PraktikumApplication.class, args);
     }
